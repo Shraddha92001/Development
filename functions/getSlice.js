2c3c151cb316737a0db95360e1549b1982976fd8
shraddha-singh <shraddhasingh92001@gmail.com>
2024-04-26 22:25:39 +0530

    slicing

---
+++ functions/getSlice.js
@@ -0,0 +1,9 @@
+// getSlice function
+
+
+function getSlice(str, start, end){
+    console.log("String:", str);
+    console.log("after slice:", str.slice(start,end));
+}
+
+getSlice("hellooooooo im shree", 2,8)
