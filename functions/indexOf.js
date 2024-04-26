3c7f808250f4c972aea3d3551f9c73090008913d
shraddha-singh <shraddhasingh92001@gmail.com>
2024-04-26 20:59:59 +0530

    find the in index and length

---
+++ functions/indexOf.js
@@ -0,0 +1,7 @@
+//indexOf function
+function index(str, target){
+    console.log("original string:" , str);
+    console.log("Index", str.indexOf(target));
+}
+index("im rahul,hello shree", "shree")
+// console.log(findIndexOf);
---
+++ functions/length.js
@@ -0,0 +1,4 @@
+// length function
+const spr = "hello";
+
+console.log(spr.length);
