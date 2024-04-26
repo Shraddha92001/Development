b9ffe4d1fbc1758851eede4111f83b2c980d2566
shraddha-singh <shraddhasingh92001@gmail.com>
2024-04-26 21:09:10 +0530

    find the last index

---
+++ functions/lastIndexOf.js
@@ -0,0 +1,6 @@
+function LastIndex( str, target){
+    console.log("the string:" , str);
+    console.log("the target:", str.lastIndexOf(target));
+}
+
+LastIndex("hello this is shree shreee  shri shreee shreee shri:" , "shri");
